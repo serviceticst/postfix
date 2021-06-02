67ea2d0e22a847af35c30c23011620b01bc8d1c0
serviceticst <70910492+serviceticst@users.noreply.github.com>
2021-06-02 11:37:56 -0300

    Add files via upload

---
+++ Install_postfix_automatizado.sh
@@ -0,0 +1,77 @@
+echo "#-----------------------------------------#"
+echo    "INFORMAÇÕES SOBRE A CONTA DE E-MAIL"
+echo "#-----------------------------------------#"
+read -p "Digite o e-mail que fará o envio: " email
+read -p "Digite o servidor SMTP: " server
+read -p "Digite a porta do servidor SMTP: " porta
+read -s -p "Digite a senha do e-mail que fará o envio: " senha
+read -p "Digite o e-mail de recebimento do teste: " emailteste
+clear
+echo "#-----------------------------------------#"
+echo            "INSTALANDO POSTFIX"
+echo "#-----------------------------------------#"
+yum install postfix mailx cyrus-sasl-plain -y
+clear
+echo "#-----------------------------------------#"
+echo       "CONFIGURANDO ARQUIVO MAIN.CF"
+echo "#-----------------------------------------#"
+mv /etc/postfix/main.cf /etc/postfix/main.cf_bkp
+touch /etc/postfix/main.cf
+cat <<EOF | /etc/postfix/main.cf
+#SMTP relayhost
+relayhost = [$server]:$porta
+# TLS Settings
+smtp_tls_loglevel = 1
+smtp_use_tls = yes
+smtpd_tls_received_header = yes
+# TLS
+smtp_sasl_auth_enable = yes
+smtp_sasl_password_maps = hash:/etc/postfix/sasl_passwd
+smtp_sasl_security_options = noanonymous
+smtp_sasl_tls_security_options = noanonymous
+queue_directory = /var/spool/postfix
+meta_directory = /etc/postfix
+setgid_group = postdrop
+command_directory = /usr/sbin
+sample_directory = /usr/share/doc/postfix/samples
+newaliases_path = /usr/bin/newaliases
+mailq_path = /usr/bin/mailq
+readme_directory = /usr/share/doc/postfix/README_FILES
+sendmail_path = /usr/sbin/sendmail
+mail_owner = postfix
+daemon_directory = /usr/libexec/postfix
+manpage_directory = /usr/share/man
+html_directory = no
+data_directory = /var/lib/postfix
+shlib_directory = /usr/lib64/postfix
+inet_protocols = ipv4
+EOF
+clear
+echo "#-----------------------------------------#"
+echo      "CONFIGURANDO ARQUIVO sasl_passwd"
+echo "#-----------------------------------------#"
+touch /etc/postfix/sasl_passwd
+[$server]:$porta $email:$senha
+clear
+echo "#-----------------------------------------#"
+echo          "AJUSTANDO AS PERMISSÕES"
+echo "#-----------------------------------------#"
+postmap /etc/postfix/sasl_passwd
+chmod 644 /etc/postfix/sasl_passwd
+chown root:postfix /etc/postfix/sasl_passwd
+systemctl restart postfix 
+clear
+echo "#-----------------------------------------#"
+echo         "ENVIANDO E-MAIL DE TESTE"
+echo "#-----------------------------------------#"
+echo "This is a test." | mail -s "test message" -r $email $emailteste
+clear
+echo "#-----------------------------------------#"
+echo             "VERIFICANDO LOG"
+echo "#-----------------------------------------#"
+tail -f /var/log/maillog
+clear
+echo "#-----------------------------------------#"
+echo        "CONSULTE SUA CAIXA DE E-MAIL"
+echo "#-----------------------------------------#"
+echo "FIM"
